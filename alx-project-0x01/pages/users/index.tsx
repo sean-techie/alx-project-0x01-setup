e38aa901a66a98091dac100eaf39f2b7953a5ea1
Reseanne Rampou <reseanner@gmail.com>
2025-11-13 20:05:35 +0200

    Updated pages/users/index.tsx

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -2,28 +2,29 @@ import Header from "@/components/layout/Header";
 import UserCard from "@/components/common/UserCard";
 import { UserProps } from "@/interfaces";
 
-const Users: React.FC<{ users: UserProps[] }> = ({ users }) => {
+const Users: React.FC<{ posts: UserProps[] }> = ({ posts }) => {
   return (
     <div className="flex flex-col h-screen">
       <Header />
       <main className="p-4">
         <h1 className="text-2xl font-semibold mb-4">Users</h1>
         <div className="grid grid-cols-3 gap-4">
-          {users?.map((user, index) => (
+          {posts?.map((user, index) => (
             <UserCard {...user} key={index} />
           ))}
         </div>
       </main>
     </div>
   );
-}
+};
+
 
 export async function getStaticProps() {
   const response = await fetch("https://jsonplaceholder.typicode.com/users");
-  const users = await response.json();
+  const posts = await response.json(); 
 
   return {
-    props: { users }
+    props: { posts }
   };
 }
 
