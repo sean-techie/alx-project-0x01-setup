d23ed3ac9c024c58a4f1286a21aeffa7be13bb16
Reseanne Rampou <reseanner@gmail.com>
2025-11-14 14:57:58 +0200

    updated interfaces/index.ts

--- alx-project-0x01/interfaces/index.ts
+++ alx-project-0x01/interfaces/index.ts
@@ -1,35 +1,11 @@
-export interface PostProps {
-  userId: number;
-  id: number;
-  title: string;
-  body: string;
-}export interface PostProps {
-  userId: number;
-  id: number;
+export interface PostData {
+  id?: number;
   title: string;
   body: string;
 }
 
-export interface UserProps {
-  id: number;
-  name: string;
-  username: string;
-  email: string;
-  address: {
-    street: string;
-    suite: string;
-    city: string;
-    zipcode: string;
-    geo: {
-      lat: string;
-      lng: string;
-    };
-  };
-  phone: string;
-  website: string;
-  company: {
-    name: string;
-    catchPhrase: string;
-    bs: string;
-  };
+export interface PostModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  onSubmit: (post: PostData) => void;
 }
